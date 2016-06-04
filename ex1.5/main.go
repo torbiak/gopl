d6a54c24de0d8313c7fdd3674cd04ccc56615cb7
Jordan Torbiak <torbiak@gmail.com>
2016-06-04 04:29:30 -0600

    done ex1.5

--- ex1.5/main.go
+++ ex1.5/main.go
@@ -20,11 +20,11 @@ import (
 )
 
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0, 255, 0, 255}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	blackIndex = 0 // first color in palette
+	greenIndex = 1 // next color in palette
 )
 
 func main() {
@@ -62,7 +62,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				greenIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
