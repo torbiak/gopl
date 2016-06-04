413f6a19abece1d3dad4f40afc955141ac12e60c
Jordan Torbiak <torbiak@gmail.com>
2016-06-04 03:59:38 -0600

    done ex1.1

--- ex1.1/main.go
+++ ex1.1/main.go
@@ -1,4 +1,3 @@
-// Echo1 prints its command-line arguments.
 package main
 
 import (
@@ -7,11 +6,5 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(os.Args)
 }
-
