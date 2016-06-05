3c9e22cb15e4f23abb07bb790ecd4207a9968c2b
Jordan Torbiak <torbiak@gmail.com>
2016-06-05 11:29:46 -0600

    start ex1.8

---
+++ ex1.8/main.go
@@ -0,0 +1,25 @@
+// ex1.7 streams the content found at each specified URL to stdout.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
