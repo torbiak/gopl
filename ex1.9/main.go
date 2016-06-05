927c5e832bbe58ba0ae0ca4c7d1023eb80e04276
Jordan Torbiak <torbiak@gmail.com>
2016-06-05 11:34:16 -0600

    start ex1.9

---
+++ ex1.9/main.go
@@ -0,0 +1,30 @@
+// ex1.8 streams the content found at each specified URL to stdout, appending
+// http:// to arguments as needed.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
