fd93e3785ce60eb3fa5398ac6e0e40f6b9c2da7e
Jordan Torbiak <torbiak@gmail.com>
2016-06-07 00:15:23 -0600

    done ex3.3

--- ex3.3/main.go
+++ ex3.3/main.go
@@ -1,9 +1,11 @@
-// ex3.1 prints an svg image, ignoring non-finite vertexes.
+// ex3.3 prints an svg image, coloring its vertices based on their height.
 package main
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
 )
 
 const (
@@ -17,8 +19,9 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+func svg(w io.Writer) {
+	zmin, zmax := minmax()
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
@@ -30,11 +33,74 @@ func main() {
 			if math.IsNaN(ax) || math.IsNaN(ay) || math.IsNaN(bx) || math.IsNaN(by) || math.IsNaN(cx) || math.IsNaN(cy) || math.IsNaN(dx) || math.IsNaN(dy) {
 				continue
 			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			fmt.Fprintf(w, "<polygon style='stroke: %s; fill: #222222' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				color(i, j, zmin, zmax), ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintln(w, "</svg>")
+}
+
+func main() {
+	svg(os.Stdout)
+}
+
+// minmax returns the min and max values for z given the min/max values of x
+// and y and assuming a square domain.
+func minmax() (min float64, max float64) {
+	min = math.NaN()
+	max = math.NaN()
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			for xoff := 0; xoff <= 1; xoff++ {
+				for yoff := 0; yoff <= 1; yoff++ {
+					x := xyrange * (float64(i+xoff)/cells - 0.5)
+					y := xyrange * (float64(j+yoff)/cells - 0.5)
+					z := f(x, y)
+					if math.IsNaN(min) || z < min {
+						min = z
+					}
+					if math.IsNaN(max) || z > max {
+						max = z
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+func color(i, j int, zmin, zmax float64) string {
+	min := math.NaN()
+	max := math.NaN()
+	for xoff := 0; xoff <= 1; xoff++ {
+		for yoff := 0; yoff <= 1; yoff++ {
+			x := xyrange * (float64(i+xoff)/cells - 0.5)
+			y := xyrange * (float64(j+yoff)/cells - 0.5)
+			z := f(x, y)
+			if math.IsNaN(min) || z < min {
+				min = z
+			}
+			if math.IsNaN(max) || z > max {
+				max = z
+			}
+		}
+	}
+
+	color := ""
+	if math.Abs(max) > math.Abs(min) {
+		red := math.Exp(math.Abs(max)) / math.Exp(math.Abs(zmax)) * 255
+		if red > 255 {
+			red = 255
+		}
+		color = fmt.Sprintf("#%02x0000", int(red))
+	} else {
+		blue := math.Exp(math.Abs(min)) / math.Exp(math.Abs(zmin)) * 255
+		if blue > 255 {
+			blue = 255
+		}
+		color = fmt.Sprintf("#0000%02x", int(blue))
+	}
+	return color
 }
 
 func corner(i, j int) (float64, float64) {
