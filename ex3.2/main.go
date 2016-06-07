f0aca4193978bf67eb9698f3c76b8fbb232657e4
Jordan Torbiak <torbiak@gmail.com>
2016-06-07 00:14:09 -0600

    done ex3.2

--- ex3.2/main.go
+++ ex3.2/main.go
@@ -1,14 +1,16 @@
-// ex3.1 prints an svg image, ignoring non-finite vertexes.
+// ex3.2 prints an SVG rendering of a 3-D surface function.
 package main
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
 )
 
 const (
 	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
+	cells         = 40                 // number of grid cells
 	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
 	zscale        = height * 0.4        // pixels per z unit
@@ -17,27 +19,29 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+type zFunc func(x, y float64) float64
+
+func svg(w io.Writer, f zFunc) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay := corner(i+1, j, f)
+			bx, by := corner(i, j, f)
+			cx, cy := corner(i, j+1, f)
+			dx, dy := corner(i+1, j+1, f)
 			if math.IsNaN(ax) || math.IsNaN(ay) || math.IsNaN(bx) || math.IsNaN(by) || math.IsNaN(cx) || math.IsNaN(cy) || math.IsNaN(dx) || math.IsNaN(dy) {
 				continue
 			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			fmt.Fprintf(w, "<polygon style='stroke: %s; fill: #222222' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				"#666666", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintln(w, "</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int, f zFunc) (float64, float64) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
@@ -51,7 +55,33 @@ func corner(i, j int) (float64, float64) {
 	return sx, sy
 }
 
-func f(x, y float64) float64 {
-	r := math.Hypot(x, y) // distance from (0,0)
-	return math.Sin(r) / r
+func eggbox(x, y float64) float64 {
+	return 0.2 * (math.Cos(x) + math.Cos(y))
+}
+
+func saddle(x, y float64) float64 {
+	a := 25.0
+	b := 17.0
+	a2 := a*a
+	b2 := b*b
+	return (y*y/a2 - x*x/b2)
+}
+
+func main() {
+	usage :=  "usage: ex3.2 saddle|eggbox"
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	var f zFunc
+	switch os.Args[1] {
+	case "saddle":
+		f = saddle
+	case "eggbox":
+		f = eggbox
+	default:
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	svg(os.Stdout, f)
 }
