346ccd9710e39bb17268832f8411cef44c8cb809
Jordan Torbiak <torbiak@gmail.com>
2016-06-06 22:12:03 -0600

    start ex2.4

---
+++ ex2.4/popcount_test.go
@@ -0,0 +1,50 @@
+// ex2.3: compare popcount implementations: looping and single-expression
+// bytewise table lookups.
+package popcount
+
+import (
+	"testing"
+)
+
+func PopCountTableLoop(x uint64) int {
+	sum := 0
+	for i := 0; i < 8; i++ {
+		sum += int(pc[byte(x>>uint(i))])
+	}
+	return sum
+}
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCountTable(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func bench(b *testing.B, f func(uint64) int) {
+	for i := 0; i < b.N; i++ {
+		f(uint64(i))
+	}
+}
+
+func BenchmarkTable(b *testing.B) {
+	bench(b, PopCountTable)
+}
+
+func BenchmarkTableLoop(b *testing.B) {
+	bench(b, PopCountTableLoop)
+}
