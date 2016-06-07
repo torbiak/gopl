e95eff0e80786b9c9cc68b9e38ea6ac339ef6325
Jordan Torbiak <torbiak@gmail.com>
2016-06-06 22:15:42 -0600

    start ex2.5

---
+++ ex2.5/popcount_test.go
@@ -0,0 +1,68 @@
+// ex2.4: compare popcount implementations:
+// - looping table lookups
+// - single-expression table lookups
+// - shift value
+package popcount
+
+import (
+	"testing"
+)
+
+func PopCountTableLoop(x uint64) int {
+	sum := 0
+	for i := 0; i < 8; i++ {
+		sum += int(pc[byte(x>>uint(i))])
+	}
+	return sum
+}
+
+func PopCountShiftValue(x uint64) int {
+	count := 0
+	mask := uint64(1)
+	for i := 0; i < 64; i++ {
+		if x&mask > 0 {
+			count++
+		}
+		x >>= 1
+	}
+	return count
+}
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCountTable(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func bench(b *testing.B, f func(uint64) int) {
+	for i := 0; i < b.N; i++ {
+		f(uint64(i))
+	}
+}
+
+func BenchmarkTable(b *testing.B) {
+	bench(b, PopCountTable)
+}
+
+func BenchmarkTableLoop(b *testing.B) {
+	bench(b, PopCountTableLoop)
+}
+
+func BenchmarkShiftValue(b *testing.B) {
+	bench(b, PopCountShiftValue)
+}
