bdfce9af1b5ef0db0022ce5e881dc47325819a86
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 16:44:58 -0600

    done ex3.8

--- ex3.8/main.go
+++ ex3.8/main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"math/cmplx"
 	"os"
+	"math/big"
 )
 
 func main() {
@@ -23,7 +24,7 @@ func main() {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, mandelbrotBigFloat(z))
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
@@ -32,7 +33,6 @@ func main() {
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
 	const contrast = 15
-
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
@@ -40,7 +40,7 @@ func mandelbrot(z complex128) color.Color {
 			switch {
 			case n > 50: // dark red
 				return color.RGBA{100, 0, 0, 255}
-			default: 
+			default:
 				// logarithmic blue gradient to show small differences on the
 				// periphery of the fractal.
 				logScale := math.Log(float64(n))/math.Log(float64(iterations))
@@ -51,6 +51,89 @@ func mandelbrot(z complex128) color.Color {
 	return color.Black
 }
 
+func mandelbrot64(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	var v complex64
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + complex64(z)
+		if cmplx.Abs(complex128(v)) > 2 {
+			switch {
+			case n > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(n))/math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrotBigFloat(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	zR := (&big.Float{}).SetFloat64(real(z))
+	zI := (&big.Float{}).SetFloat64(imag(z))
+	var vR, vI = &big.Float{}, &big.Float{}
+	for i := uint8(0); i < iterations; i++ {
+		// (r+i)^2 = r^2 + 2ri + i^2
+		vR2, vI2 := &big.Float{}, &big.Float{}
+		vR2.Mul(vR, vR).Sub(vR2, (&big.Float{}).Mul(vI, vI)).Add(vR2, zR)
+		vI2.Mul(vR, vI).Mul(vI2, big.NewFloat(2)).Add(vI2, zI)
+		vR, vI = vR2, vI2
+		squareSum := &big.Float{}
+		squareSum.Mul(vR, vR).Add(squareSum, (&big.Float{}).Mul(vI, vI))
+		if squareSum.Cmp(big.NewFloat(4)) == 1 {
+			switch {
+			case i > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(i))/math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+
+func mandelbrotRat(z complex128) color.Color {
+	// High-resolution images take an extremely long time to render with
+	// iterations = 200. Multiplying arbitrary precision numbers has
+	// algorithmic complexity of at least O(n*log(n)*log(log(n)))
+	// (https://en.wikipedia.org/wiki/Arbitrary-precision_arithmetic#Implementation_issues).
+	const iterations = 200
+	const contrast = 15
+	zR := (&big.Rat{}).SetFloat64(real(z))
+	zI := (&big.Rat{}).SetFloat64(imag(z))
+	var vR, vI = &big.Rat{}, &big.Rat{}
+	for i := uint8(0); i < iterations; i++ {
+		// (r+i)^2 = r^2 + 2ri + i^2
+		vR2, vI2 := &big.Rat{}, &big.Rat{}
+		vR2.Mul(vR, vR).Sub(vR2, (&big.Rat{}).Mul(vI, vI)).Add(vR2, zR)
+		vI2.Mul(vR, vI).Mul(vI2, big.NewRat(2, 1)).Add(vI2, zI)
+		vR, vI = vR2, vI2
+		squareSum := &big.Rat{}
+		squareSum.Mul(vR, vR).Add(squareSum, (&big.Rat{}).Mul(vI, vI))
+		if squareSum.Cmp(big.NewRat(4, 1)) == 1 {
+			switch {
+			case i > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(i))/math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
 
 // Some other interesting functions:
 
---
+++ ex3.8/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+	"image/color"
+)
+
+func benchmarkMandelbrot(b *testing.B, f func(complex128) color.Color) {
+	for i := 0; i < b.N; i++ {
+		f(complex(float64(i), float64(i)))
+	}
+}
+
+func BenchmarkMandelbrotComplex128(b *testing.B) {
+	benchmarkMandelbrot(b, mandelbrot)
+}
+
+func BenchmarkMandelbrotComplex64(b *testing.B) {
+	benchmarkMandelbrot(b, mandelbrot64)
+}
+
+
+func BenchmarkMandelbrotBigFloat(b *testing.B) {
+	benchmarkMandelbrot(b, mandelbrotBigFloat)
+}
+
+func BenchmarkMandelbrotRat(b *testing.B) {
+	benchmarkMandelbrot(b, mandelbrotRat)
+}
