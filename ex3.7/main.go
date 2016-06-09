2064f3e3ad2f61f851a1efafc0aa93dffe32330d
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 00:27:05 -0600

    done ex3.7

--- ex3.7/main.go
+++ ex3.7/main.go
@@ -1,4 +1,6 @@
-// ex3.5 emits a full-color PNG image of the Mandelbrot fractal.
+// ex3.7 visualizes how many iterations it takes to find complex roots of a
+// quartic equation using Newtown's method, using different colors for
+// different roots.
 package main
 
 import (
@@ -9,6 +11,18 @@ import (
 	"math/cmplx"
 	"os"
 )
+
+type Func func(complex128) complex128
+
+var colorPool = []color.RGBA{
+	{170, 57, 57, 255},
+	{170, 108, 57, 255},
+	{34, 102, 102, 255},
+	{45, 136, 45, 255},
+}
+
+var chosenColors = map[complex128]color.RGBA{}
+
 
 func main() {
 	const (
@@ -23,7 +37,7 @@ func main() {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, z4(z))
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
@@ -73,14 +87,60 @@ func sqrt(z complex128) color.Color {
 // z' = z - f(z)/f'(z)
 //    = z - (z^4 - 1) / (4 * z^3)
 //    = z - (z - 1/z^3) / 4
-func newton(z complex128) color.Color {
+func z4(z complex128) color.Color {
+	f := func(z complex128) complex128 {
+		return z*z*z*z-1
+	}
+	fPrime := func(z complex128) complex128 {
+		return (z - 1/(z*z*z)) / 4
+	}
+	return newton(z, f, fPrime)
+}
+
+// f(x) = x^4 + 2x^3 + 3x^2 + 4x - 5
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 + 2z^3 + 3z^2 + 4z - 5) / (4z^3 + 6z^2 + 6z + 4)
+func quartic(z complex128) color.Color {
+	f := func(z complex128) complex128 {
+		return z*z*z*z + 2*z*z*z + 3*z*z + 4*z - 5
+	}
+	fPrime := func(z complex128) complex128 {
+		return (z*z*z*z + 2*z*z*z + 3*z*z + 4*z - 5) / (4*z*z*z + 6*z*z + 6*z + 4)
+	}
+	return newton(z, f, fPrime)
+}
+
+func newton(z complex128, f Func, fPrime Func) color.Color {
 	const iterations = 37
 	const contrast = 7
 	for i := uint8(0); i < iterations; i++ {
-		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			return color.Gray{255 - contrast*i}
+		z -= fPrime(z)
+		if cmplx.Abs(f(z)) < 1e-6 {
+			root := complex(round(real(z), 4), round(imag(z), 4))
+			c, ok := chosenColors[root]
+			if !ok {
+				if len(colorPool) == 0 {
+					panic("no colors left")
+				}
+				c = colorPool[0]
+				colorPool = colorPool[1:]
+				chosenColors[root] = c
+			}
+			// Convert to YCbCr to make producing different shades easier.
+			y, cb, cr := color.RGBToYCbCr(c.R, c.G, c.B)
+			scale := math.Log(float64(i)) / math.Log(iterations)
+			y -= uint8(float64(y) * scale)
+			return color.YCbCr{y, cb, cr}
 		}
 	}
 	return color.Black
+}
+
+func round(f float64, digits int) float64 {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	pow := math.Pow10(digits)
+	return math.Trunc(f*pow + math.Copysign(0.5, f)) / pow
 }
