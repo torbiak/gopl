67eb5a5340c34cc0b9ca3b8dde1676b52aec944b
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 19:20:23 -0600

    done ex3.10

--- ex3.10/main.go
+++ ex3.10/main.go
@@ -1,40 +1,32 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 73.
-
-// Comma prints its argument numbers with a comma at each power of 1000.
-//
-// Example:
-// 	$ go build gopl.io/ch3/comma
-//	$ ./comma 1 12 123 1234 1234567890
-// 	1
-// 	12
-// 	123
-// 	1,234
-// 	1,234,567,890
-//
+// ex3.10 inserts commas into integer strings given as command-line arguments,
+// without using recursion.
 package main
 
 import (
 	"fmt"
+	"bytes"
 	"os"
 )
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("  %s\n", comma(os.Args[i]))
+		fmt.Printf("%s\n", comma(os.Args[i]))
 	}
 }
 
-//!+
 // comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
-	n := len(s)
-	if n <= 3 {
-		return s
+	b := &bytes.Buffer{}
+	pre := len(s) % 3
+	// Write the first group of up to 3 digits.
+	if pre == 0 {
+		pre = 3
 	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+	b.WriteString(s[:pre])
+	// Deal with the rest.
+	for i := pre; i < len(s); i += 3 {
+		b.WriteByte(',')
+		b.WriteString(s[i:i+3])
+	}
+	return b.String()
 }
-
-//!-
---
+++ ex3.10/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestComma(t *testing.T) {
+	tests := []struct{
+		s, want string
+	}{
+		{"1", "1"},
+		{"12", "12"},
+		{"123", "123"},
+		{"1234", "1,234"},
+		{"12345", "12,345"},
+		{"123456", "123,456"},
+		{"1234567", "1,234,567"},
+		{"12345678", "12,345,678"},
+		{"123456789", "123,456,789"},
+		{"1234567890", "1,234,567,890"},
+	}
+	for _, test := range tests {
+		got := comma(test.s) 
+		if got != test.want {
+			t.Errorf("comma(%q), got %q, want %q", test.s, got, test.want)
+		}
+	}
+}
