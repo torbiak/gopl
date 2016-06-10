f3f9fbadea46119c3255487a9b7a51ed5d64d5be
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 23:44:14 -0600

    done ex5.4

--- ex5.4/main.go
+++ ex5.4/main.go
@@ -1,4 +1,5 @@
-// Findlinks1 prints the links in an HTML document read from standard input.
+// ex5.4 prints the links in an HTML document read from standard input,
+// including those for images, scripts, and style sheets.
 package main
 
 import (
