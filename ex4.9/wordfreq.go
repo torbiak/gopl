8183088549b64ec19db86692209c490c23818eb6
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 21:33:09 -0600

    done ex4.9

--- ex4.9/wordfreq.go
+++ ex4.9/wordfreq.go
@@ -1,3 +1,4 @@
+// ex4.9 counts word frequency for stdin.
 package main
 
 import (
@@ -13,6 +14,10 @@ func main() {
 	for scanner.Scan() {
 		word := scanner.Text()
 		freq[word]++
+	}
+	if scanner.Err() != nil {
+		fmt.Fprintln(os.Stderr, scanner.Err())
+		os.Exit(1)
 	}
 	for word, n := range freq {
 		fmt.Printf("%-30s %d\n", word, n)
