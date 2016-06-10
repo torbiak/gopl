9f27eb601e85a64bce458d720d32e605fdc5da9c
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 21:39:39 -0600

    done ex4.10

--- ex4.10/main.go
+++ ex4.10/main.go
@@ -1,74 +1,82 @@
-// Issuesreport prints a report of issues matching the search terms.
+// ex4.10 prints a table of GitHub issues matching the search terms, organized
+// by the past day, month, and year.
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"text/template"
 	"time"
 
 	"gopl.io/ch4/github"
 )
 
-const templ = `{{.TotalCount}} issues:
-{{range .Items}}----------------------------------------
-Number: {{.Number}}
-User:   {{.User.Login}}
-Title:  {{.Title | printf "%.64s"}}
-Age:    {{.CreatedAt | daysAgo}} days
-{{end}}`
-
-
-func daysAgo(t time.Time) int {
-	return int(time.Since(t).Hours() / 24)
-}
-
-
-var report = template.Must(template.New("issuelist").
-	Funcs(template.FuncMap{"daysAgo": daysAgo}).
-	Parse(templ))
-
+//!+
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := report.Execute(os.Stdout, result); err != nil {
-		log.Fatal(err)
+	format := "#%-5d %9.9s %.55s\n"
+	now := time.Now()
+
+	pastDay := make([]*github.Issue, 0)
+	pastMonth := make([]*github.Issue, 0)
+	pastYear := make([]*github.Issue, 0)
+
+	day := now.AddDate(0, 0, -1)
+	month := now.AddDate(0, -1, 0)
+	year := now.AddDate(-1, 0, 0)
+	for _, item := range result.Items {
+		switch {
+		case item.CreatedAt.After(day):
+			pastDay = append(pastDay, item)
+		case item.CreatedAt.After(month) && item.CreatedAt.Before(day):
+			pastMonth = append(pastMonth, item)
+		case item.CreatedAt.After(year) && item.CreatedAt.Before(month):
+			pastYear = append(pastYear, item)
+		}
+	}
+
+	if len(pastDay) > 0 {
+		fmt.Printf("\nPast day:\n")
+		for _, item := range pastDay {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastMonth) > 0 {
+		fmt.Printf("\nPast month:\n")
+		for _, item := range pastMonth {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastYear) > 0 {
+		fmt.Printf("\nPast year:\n")
+		for _, item := range pastYear {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
 	}
 }
 
-
-func noMust() {
-	report, err := template.New("report").
-		Funcs(template.FuncMap{"daysAgo": daysAgo}).
-		Parse(templ)
-	if err != nil {
-		log.Fatal(err)
-	}
-	result, err := github.SearchIssues(os.Args[1:])
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := report.Execute(os.Stdout, result); err != nil {
-		log.Fatal(err)
-	}
-}
+//!-
 
 /*
-$ go build gopl.io/ch4/issuesreport
-$ ./issuesreport repo:golang/go is:open json decoder
+//!+textoutput
+$ go build gopl.io/ch4/issues
+$ ./issues repo:golang/go is:open json decoder
 13 issues:
-----------------------------------------
-Number: 5680
-User:   eaigner
-Title:  encoding/json: set key converter on en/decoder
-Age:    750 days
-----------------------------------------
-Number: 6050
-User:   gopherbot
-Title:  encoding/json: provide tokenizer
-Age:    695 days
-----------------------------------------
-...
+#5680    eaigner encoding/json: set key converter on en/decoder
+#6050  gopherbot encoding/json: provide tokenizer
+#8658  gopherbot encoding/json: use bufio
+#8462  kortschak encoding/json: UnmarshalText confuses json.Unmarshal
+#5901        rsc encoding/json: allow override type marshaling
+#9812  klauspost encoding/json: string tag not symmetric
+#7872  extempora encoding/json: Encoder internally buffers full output
+#9650    cespare encoding/json: Decoding gives errPhase when unmarshalin
+#6716  gopherbot encoding/json: include field name in unmarshal error me
+#6901  lukescott encoding/json, encoding/xml: option to treat unknown fi
+#6384    joeshaw encoding/json: encode precise floating point integers u
+#6647    btracey x/tools/cmd/godoc: display type kind of each named type
+#4237  gjemiller encoding/base64: URLEncoding padding is optional
+//!-textoutput
 */
