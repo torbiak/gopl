f98dc61372487e6a8be13f4ceb5865576f072510
Jordan Torbiak <torbiak@gmail.com>
2016-06-09 20:43:06 -0600

    done ex4.3

--- ex4.3/reverse.go
+++ ex4.3/reverse.go
@@ -1,19 +1,9 @@
-package main
+// ex4.3 reverses an array
+package reverse
 
-import (
-	"fmt"
-)
-
-// ex4.3
 func reverse(ints *[5]int) {
 	for i := 0; i < len(ints)/2; i++ {
 		end := len(ints) - i - 1
 		ints[i], ints[end] = ints[end], ints[i]
 	}
-}
-
-func main() {
-	a := [...]int{1, 2, 3, 4, 5}
-	reverse(&a)
-	fmt.Println(a)
 }
---
+++ ex4.3/reverse_test.go
@@ -0,0 +1,20 @@
+package reverse
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestReverse(t *testing.T) {
+	tests := []struct{
+		a, want [5]int
+	}{
+		{[5]int{1, 2, 3, 4, 5}, [5]int{5, 4, 3, 2, 1}},
+	}
+	for _, test := range tests {
+		reverse(&test.a)
+		if !reflect.DeepEqual(test.a, test.want) {
+			t.Errorf("got %v, want %v", test.a, test.want)
+		}
+	}
+}
